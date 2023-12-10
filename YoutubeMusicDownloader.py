668150bfff09f2fe568fcb111c9aeacbecee99e2
Francis <91210651+Franspai@users.noreply.github.com>
2023-12-10 19:41:26 +0100

    Changed the way dirs are inserted into Download.py from the user-input script

--- YoutubeMusicDownloader.py
+++ YoutubeMusicDownloader.py
@@ -8,22 +8,10 @@ def check_existing_links():
             existing_links.update(file.read().split(","))
     return existing_links
 
-def update_download_script(download_dir):
-    with open("Download.py", "r") as file:
-        script_content = file.readlines()
-
-    updated_script_content = []
-    for line in script_content:
-        if line.startswith("download_directory"):
-            updated_script_content.append(f'download_directory = "{download_dir}"\n')
-        else:
-            updated_script_content.append(line)
-
-    with open("Download.py", "w") as file:
-        file.writelines(updated_script_content)
-
 def main():
     existing_links = check_existing_links()
+
+    download_dir = input("Enter the download directory (press Enter to skip): ")
 
     while True:
         artist_link = input("Enter the artist's YouTube Music playlist link (or 'n' to stop adding): ")
@@ -33,7 +21,7 @@ def main():
             if add_more.lower() == 'n':
                 break
             elif add_more.lower() == 'd':
-                subprocess.run(["python3", "Download.py"])
+                subprocess.run(["python", "Download.py", download_dir])
             continue
 
         # Check if it's a valid YouTube Music playlist link
@@ -49,8 +37,19 @@ def main():
                 file.write(artist_link + ",")
                 print("Artist's songs link added to artists.txt")
 
-        download_dir = input("Enter the download directory: ")
-        update_download_script(download_dir)
+    if download_dir:
+        with open("Download.py", "r") as file:
+            script_content = file.readlines()
+
+        updated_script_content = []
+        for line in script_content:
+            if line.startswith("download_directory"):
+                updated_script_content.append(f'download_directory = "{download_dir}"\n')
+            else:
+                updated_script_content.append(line)
+
+        with open("Download.py", "w") as file:
+            file.writelines(updated_script_content)
 
 if __name__ == "__main__":
     main()
